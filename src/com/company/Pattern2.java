37a901d1972c18a4db01e94fb9eac9356f78f22d
Diksh <diksh.sawaliya@gmail.com>
2025-01-29 01:33:13 +0530

    Added pattern3

--- src/com/company/Pattern2.java
+++ src/com/company/Pattern2.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Patter2 {
+public class Pattern2 {
     
     // * * *
     // * * *
---
+++ src/com/company/Pattern3.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Pattern3 {
+    
+
+    // * * * *
+    // * * *
+    // * *
+    // *
+
+
+    static void pattern3(int n){
+
+        //for every row that is printing 
+        for(int row = 1; row <= n; row ++){
+            //column will be printing inversely
+            for(int col = 1; col <= n-row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern3(5);
+    }
+}
