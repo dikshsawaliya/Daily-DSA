f2ee933e001fd6afda988b69920c449c0205fa41
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2025-12-23 22:01:11 +0530

    Three Sum solution for Leetcode

---
+++ src/com/company/leetcode/ThreeSum.java
@@ -0,0 +1,61 @@
+package com.company.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int [] nums = {-4,-1,1,0,1,2};
+        System.out.println(threeSum(nums));
+    }
+
+
+    static List<List<Integer>> threeSum(int[]nums){
+
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> integerList = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length -2; i++) {
+
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+
+            int left = i+1;
+            int right = nums.length -1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    integerList.add(nums[i]);
+                    integerList.add(nums[left]);
+                    integerList.add(nums[right]);
+                    list.add(integerList);
+
+                while (nums[i] > 0 && nums[left] == nums[left + 1]) {
+                    left++;
+                }
+
+                while (nums[i] > 0 && nums[right] == nums[right - 1]) {
+                    right--;
+                }
+                    left++;
+                    right--;
+
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+
+            }
+        }
+
+        return list;
+    }
+}
