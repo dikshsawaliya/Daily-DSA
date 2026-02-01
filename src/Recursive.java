67d5734e25060c62525a3156c89e0056fd8ac15d
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2026-02-02 00:55:36 +0530

    Added second easy recursive problem

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,7 +1,8 @@
-public class SumOfFirstNaturalNumbers {
+public class Recursive {
 
     public static void main(String[] args) {
         System.out.println(sumOfFirstNaturalNumbers(10000));
+        System.out.println(powerOfN(2,4));
     }
 
     static long sumOfFirstNaturalNumbers(long n){
@@ -9,6 +10,16 @@ public class SumOfFirstNaturalNumbers {
             return n;
         }
         return n + sumOfFirstNaturalNumbers(n-1);
+    }
+
+
+    static int powerOfN(int a, int b){
+
+        if(a == 0 || b == 0){
+            return 1;
+        }
+
+        return a * powerOfN(a, b-1);
     }
 
 }
