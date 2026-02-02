90161384559f7f11622b8a6924e6a4dbafc202d7
Diksh <diksh.sawaliya@gmail.com>
2026-02-03 02:07:48 +0530

    Updated the function LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,17 +1,52 @@
 public class LinkedList {
 
+    //  1 -> 2 -> 3 -> 4
+    // 1 -> null
+
     public static void main(String[] args) {
         Node first = new Node(1);
         Node second = new Node(2);
         Node third = new Node(3);
 
-
         first.next = second;
         second.next = third;
 
+        inserAtEnd(4, third);
+        first = addInBegining(0, first);
         printNode(first);
 
     }
+
+    static Node addInBegining(int data, Node head){
+
+        Node node = new Node(data);
+        node.next = head;
+        return node;
+    }
+
+
+    static Node inserAtEnd(int data, Node head){
+
+        Node node = new Node(data);
+
+        if(head == null){
+            return node;
+        }
+
+        head.next = node;
+        return node;
+
+    }
+
+
+    // static void addInBetween(Node node, int newData){
+
+    //     int temp;
+    //     temp = node.next.data;
+    //     node.next.data = newData;
+    //     node.next
+
+    // }
 
 
     static void printNode(Node node){
@@ -27,7 +62,6 @@ public class LinkedList {
 }
 
 class Node {
-
     int data;
     Node next;
 
