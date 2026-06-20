43b7126fecb77fb475bd5e45063473bab119a7b7
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2026-06-21 01:16:08 +0530

    Updated Strivers pattern

---
+++ src/com/company/Pattern/StriverPatterns.java
@@ -0,0 +1,28 @@
+package com.company.Pattern;
+
+public class StriverPatterns {
+
+    public static void main(String[] args) {
+//        boxStars(5);
+        stairsAscending(5);
+    }
+
+    static void boxStars(int n ){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static void stairsAscending(int n ){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
